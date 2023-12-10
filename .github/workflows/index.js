a40ec048e7c83bfaa27839da099266fe1da9deea
kopzif <144530450+kopzif@users.noreply.github.com>
2023-12-11 02:31:30 +0330

    Add files via upload

---
+++ .github/workflows/index.js
@@ -0,0 +1,46 @@
+/*www.almasweb.org*/
+
+function showLogin() {
+  $(".login-link").addClass("active");
+  $(".signup-link").removeClass("active");
+  $(".btn-login").text("ورود");
+  $(".confirm-password-row").hide()
+  $(".forgot-password-row").show();
+}
+
+function showSignup() {
+  $(".login-link").removeClass("active");
+  $(".signup-link").addClass("active");
+  $(".btn-login").text("عضویت");
+  $(".confirm-password-row").show()
+  $(".forgot-password-row").hide();
+}
+
+function checkPasswordStrength() {
+  if (!$("#password").val() || $(".login-link").hasClass("active")) return;
+
+  const emoji = {
+    0: "\u{1F628}", // Fearful 😨
+    1: "\u{1F616}", // Confounded 😖
+    2: "\u{1F61E}", // Disappointed 😞
+    3: "\u{1F615}", // Confused 😕
+    4: "\u{1F603}" // Grinning 😃
+  };
+  const result = zxcvbn($("#password").val());
+  const warning = result.feedback.warning || "";
+  const suggestion = result.feedback.suggestions.join(", ").replace(/,/g, "") || "";
+  
+  $("#password-strength").html(emoji[result.score]);
+  $(".help-text").text(`سعی کنید پسوردتان بهتر شود !`);
+}
+
+function init() {
+  $(".login-link").on("click", showLogin);
+  $(".signup-link").on("click", showSignup);
+  $("#password")
+    .on("input focus", checkPasswordStrength)
+    .on("blur", () => $("#password-strength, .help-text").empty());
+}
+
+$(init);
+/*www.almasweb.org*/
